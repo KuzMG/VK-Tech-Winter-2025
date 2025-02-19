181f81bd559cedcd63749f5a49db7ee030035171
Mikhail Kuznetsov <mifa_224@mail.ru>
2025-02-19 13:24:21 +0300

    Initial project version

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "VK-Tech-Winter-2025"
+include(":app")
+ 
